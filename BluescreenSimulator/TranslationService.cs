3e8a7e47771e534c06f1997d2f7e9019da5445fc
Roco <ryxn@ryxn.xyz>
2020-07-22 05:28:41 -0400

    squished tiny bugs

--- BluescreenSimulator/TranslationService.cs
+++ BluescreenSimulator/TranslationService.cs
@@ -13,37 +13,36 @@ namespace BluescreenSimulator
         private const string SubKey = "";
         private const string Endpoint = "https://api.cognitive.microsofttranslator.com/";
 
-        public async Task<object> Translate(string From, string To)
+        public Task<string> Translate(string From, string To)
         {
-            return await TranslationRequest(To, From);
+            To = $"/translate?api-version=3.0&to={To}";
+            return TranslationRequest(To, From);
         }
 
-        public async Task<object> TranslationRequest(string route, string text)
+        public async Task<string> TranslationRequest(string route, string text)
         {
-            object[] body = new[] {new {Text = text}};
+            object[] body = {new {Text = text}};
             var requestBody = JsonConvert.SerializeObject(body);
             using (var client = new HttpClient())
+            using (var request = new HttpRequestMessage())
             {
-                using (var request = new HttpRequestMessage())
+                request.Method = HttpMethod.Post;
+                request.RequestUri = new Uri(Endpoint + route);
+                request.Content = new StringContent(requestBody,Encoding.UTF8,"application/json");
+                request.Headers.Add("Ocp-Apim-Subscription-Key", SubKey);
+
+                var response = await client.SendAsync(request).ConfigureAwait(false);
+                var results = await response.Content.ReadAsStringAsync();
+                var readableOutput =
+                    JsonConvert.DeserializeObject<JsonParse.TranslationResult[]>(results);
+                foreach (var o in readableOutput)
                 {
-                    request.Method = HttpMethod.Post;
-                    request.RequestUri = new Uri(Endpoint + route);
-                    request.Content = new StringContent(requestBody,Encoding.UTF8,"application/json");
-                    request.Headers.Add("Ocp-Apim-Subscription-Key", SubKey);
-
-                    var response = await client.SendAsync(request).ConfigureAwait(false);
-                    var results = await response.Content.ReadAsStringAsync();
-                    var readableOutput =
-                        JsonConvert.DeserializeObject<JsonParse.TranslationResult[]>(results);
-                    foreach (var o in readableOutput)
+                    foreach (var t in o.Translations)
                     {
-                        foreach (var t in o.Translations)
-                        {
-                            return t.Text; 
-                        }
+                        return t.Text; 
                     }
-
                 }
+
             }
 
             return null;
